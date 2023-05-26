7fcb1282d92114a437b0e9d920c8206b3d1e578b
하겸 윤 <younha3710@naver.com>
2023-05-27 00:52:13 +0900

    commit

---
+++ cppStudy/increaseBy.cpp
@@ -0,0 +1,23 @@
+// 명품 C++ 프로그래밍 5장 실습문제 5번
+#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int r) { radius = r; }
+	int getRadius() { return radius; }
+	void setRadius(int r) { radius = r; }
+	void show() { cout << "반지름이 " << radius << "인 원" << endl; }
+};
+
+void increaseBy(Circle& a, Circle& b) { // Circle 객체 b를 a에 더하여 a를 키우고자 하는 함수
+	int r = a.getRadius() + b.getRadius();
+	a.setRadius(r);
+}
+
+int main() {
+	Circle x(10), y(5);
+	increaseBy(x, y); // x의 반지름이 15인 원을 만들고자 한다.
+	x.show(); // "반지름이 15인 원"을 출력한다.
+}
